f4a22e78296846a37f8192813d5ecc9f8149222a
MooShaaban <shaabanmohamed479@gmail.com>
2022-02-26 16:38:28 +0200

    PC commit

--- lib/modules/login/login_design.dart
+++ lib/modules/login/login_design.dart
@@ -7,6 +7,7 @@ class LoginDesign extends StatelessWidget{
 
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -21,66 +22,86 @@ class LoginDesign extends StatelessWidget{
 
           width: double.infinity,
           padding: EdgeInsetsDirectional.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 130.0,),
-              Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.w700
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 130.0,),
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w700
+                  ),
                 ),
-              ),
-              SizedBox(height: 30.0,),
-              TextFormField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value){
-                  print(value);
-                },
-                onFieldSubmitted: (value){
-                  print(value);
-                },
-                decoration: InputDecoration(
-                  labelText: 'Email Adress',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.mail_outline),
-
-                ),
-              ),
-              SizedBox(height: 10.0,),
-              TextFormField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.lock),
-                  suffixIcon: Icon(Icons.remove_red_eye),
-
-
-                ),
-              ),
-              SizedBox(height: 10.0,),
-              defaultButton(
-                  text: 'Login',
-                  function: (){
-                    print(emailController.text) ;
-                    print(passwordController.text);
+                SizedBox(height: 30.0,),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value){
+                    print(value);
                   },
-                backgroundColor: Colors.deepPurpleAccent,
-              ),
-              SizedBox(height: 10.0,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Don\'t have an account ?'),
-                  TextButton(onPressed: (){}, child: Text('Register Now'))
+                  onFieldSubmitted: (value){
+                    print(value);
+                  },
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return 'Email address must not be empty';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Email Adress',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.mail_outline),
 
-                ],
-              )
-            ],
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.remove_red_eye),
+
+
+                  ),
+                  validator: (value){
+                    if (value!.isEmpty){
+                      return 'Password must not be empty';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10.0,),
+                defaultButton(
+                  radius: 15.0,
+                  isUpperCase: false,
+                    text: 'Login',
+                    function: (){
+                      if(formKey.currentState!.validate()){
+                        print(emailController.text) ;
+                        print(passwordController.text);
+                      }
+
+                    },
+                  backgroundColor: Colors.deepPurpleAccent,
+                ),
+                SizedBox(height: 10.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have an account ?'),
+                    TextButton(onPressed: (){}, child: Text('Register Now'))
+
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,24 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 Widget defaultButton ({
   required String text,
   Color backgroundColor = Colors.blue,
-  required Function function,
+  required Function() function,
   double height = 40.0,
-  double width = double.infinity
+  double width = double.infinity,
+  bool isUpperCase = true,
+  double radius = 0,
 
 
 }) {
 
-     return MaterialButton(
-        minWidth: width,
-        height: height,
-        child: Text(text,
-          style: TextStyle(
-            color: Colors.black,
+     return Container(
+       height: height,
+       width: width,
+       decoration: BoxDecoration(
+         borderRadius: BorderRadius.circular(radius),
+         color: backgroundColor,
+       ),
+       child: MaterialButton(
+          child: Text(isUpperCase? text.toUpperCase() : text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
+
+          onPressed: function,
         ),
-        color: backgroundColor,
-        onPressed: function(),
-      );
+     );
 }
