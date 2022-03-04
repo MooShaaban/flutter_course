d82e463d7d337ff430aee986b683e3abda897622
MooShaaban <shaabanmohamed479@gmail.com>
2022-03-04 14:41:36 +0200

    PC commit

--- lib/layout/todo_home.dart
+++ lib/layout/todo_home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:remontada2/modules/archived_todo/archived_todo.dart';
 import 'package:remontada2/modules/done_todo/done_todo.dart';
 import 'package:remontada2/modules/tasks_todo/new_tasks.dart';
+import 'package:sqflite/sqflite.dart';
+
 
 class TodoHome extends StatefulWidget {
   const TodoHome({Key? key}) : super(key: key);
@@ -13,6 +15,7 @@ class TodoHome extends StatefulWidget {
 class _TodoHomeState extends State<TodoHome> {
 
   int current = 0;
+  Database? database;
 
   List<String> title = [
     'New Tasks',
@@ -27,6 +30,12 @@ class _TodoHomeState extends State<TodoHome> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+
+    createDatabase();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -34,8 +43,29 @@ class _TodoHomeState extends State<TodoHome> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          print(await getName());
-        },
+          insertToDatabase();
+        }
+
+      //       try{
+      //   print(await getName());
+      //   print('Ahmed Ali');
+      //   throw('Error!!!!!!');
+      //   }catch(error){
+      // print('Error is ${error.toString()}');
+      // };
+
+
+        //The .then method below
+      //     (){
+      // getName().then((value) {
+      // print(value);
+      // print('Ahmed Ali');
+      // throw('Error!!!!!!');
+      // }).catchError((error){
+      // print('error is ${error.toString()}');
+      // });
+      // },
+        ,
         child: Icon(Icons.add),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -72,6 +102,42 @@ class _TodoHomeState extends State<TodoHome> {
   Future<String> getName() async  {
     return 'Mohamed Shaaban';
   }
+
+  void createDatabase() async {
+     database = await openDatabase(
+        'todo.db',
+      version: 1,
+      onCreate: (database, version)  {
+          print('Database Created');
+          database.execute(
+              'CREATE TABLE tasks(id INTEGER PRIMARY KEY,title TEXT ,date TEXT,time TEXT,status TEXT )',
+
+          ).then((value) {
+            print('Table created');
+          }).catchError((error){
+            print('Error is $error');
+          });
+      },
+      onOpen: (database){
+          print('Database opened');
+      }
+    );
+  }
+  void insertToDatabase(){
+    database!.transaction((txn) async{
+      try{
+        int id = await  txn.rawInsert('INSERT INTO tasks (title, date, time, status) VALUES ("first task","14 May","midnight","done")');
+        print('$id inserted successfully');
+      }catch(error){
+        print('error while inserting new record is ${error.toString()}');
+      }
+
+
+
+    });
+  }
+
+
 }
 
 
