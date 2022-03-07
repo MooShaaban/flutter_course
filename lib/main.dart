f89c0338ec3c522b830e89298b2d2f7fe8c41539
MooShaaban <shaabanmohamed479@gmail.com>
2022-03-07 17:08:01 +0200

    PC commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:remontada2/modules/home/home_screen.dart';
 import 'package:remontada2/modules/users/users_screen.dart';
-import 'package:remontada2/modules/sws_home.dart';
+import 'package:remontada2/sws_project/sws_home.dart';
 import 'layout/todo_home.dart';
 import 'modules/bmi_result/bmi_result_screen.dart';
 import 'modules/bmi/bmi_screen.dart';
---
+++ lib/sws_project/sws_home.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:remontada2/sws_project/mainstream_building.dart';
+
+import 'credit_building.dart';
+
+class SWSHome extends StatefulWidget {
+  const SWSHome({Key? key}) : super(key: key);
+
+  @override
+  State<SWSHome> createState() => _SWSHomeState();
+}
+
+class _SWSHomeState extends State<SWSHome> {
+
+  int screenIndex = 0;
+  List<Widget> screen =[
+    CreditBuilding(),
+    MainStreamBuilding(),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+
+        padding: EdgeInsetsDirectional.all(12.0),
+
+
+
+        child: Material(
+          color: Colors.transparent,
+          elevation: 10.0,
+          child: Container(
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              color: Colors.transparent,
+            ),
+            child: BottomNavigationBar(
+
+
+              selectedFontSize: 23.0,
+              unselectedFontSize: 15.0,
+              type: BottomNavigationBarType.fixed,
+              currentIndex: screenIndex,
+              onTap: (index){
+                setState(() {
+                  screenIndex = index;
+                });
+
+              },
+              fixedColor: Colors.deepPurple[500],
+              items: [
+                BottomNavigationBarItem(
+                  icon: Container(
+                    height: 0.0,
+                    width: 0.0,
+                  ),
+                  title:Text(
+                    'Credit Building',
+                    textAlign: TextAlign.center,
+                    maxLines: 2,
+                  ),
+                ),
+                BottomNavigationBarItem(
+                    icon:  Container(
+                      height: 0.0,
+                      width: 0.0,
+                    ),
+                    title: Text(
+                      'Mainstream Building',
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                    )
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: screen[screenIndex],
+
+    );
+  }
+}
